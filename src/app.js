759418714531d78f08273dac7ad3c0e44bab6d8b
zhangrongjie <zhangrongjie01@dfwsgroup.com>
2021-12-01 14:08:16 +0800

    首次安装parcel打包vue组件

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 import Vue from 'vue';
 import Button from './button.vue'
 
+console.log(Button)
+
 Vue.component('g-button', Button)
 
+// 在最顶层必须要调用render方法，不然渲染不出来.
 new Vue({
   el: '#app',
   data: {
 
-  }
+  },
+  render:c=>c(Button)
 })
