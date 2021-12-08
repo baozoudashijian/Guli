4fb0d87f68b39894ca2b491e68260f5f96f0e6fc
baozoudashijian <Yhldcahcy521>
2021-12-08 09:48:24 +0800

    完成input测试用例

--- test/button.test.js
+++ test/button.test.js
@@ -18,11 +18,9 @@ describe('Button', () => {
         icon: 'setting'
       }
     }).$mount()
-    console.log(vm)
     // 我只是将这个icon放在了内存中，并没有放进document（页面中）
     // const useElement = document.querySelector('use')
     const useElement = vm.$el.querySelector('use')
-    console.log(useElement)
     expect(useElement.getAttribute('xlink:href')).to.equal('#icon-setting')
     vm.$destroy()
   })
--- test/input.test.js
+++ test/input.test.js
@@ -1,3 +1,5 @@
+import {use} from "chai/index";
+
 const expect = chai.expect;
 import Vue from 'vue';
 import Input from '../src/input.vue';
@@ -7,5 +9,86 @@ describe('Input', () => {
   it('存在.', () => {
     expect(Input).to.exist
   })
+
+  //  测试属性
+  describe('props.', () => {
+    it('设置Value', () => {
+      const Construct = Vue.extend(Input)
+      const vm = new Construct({
+        propsData: {
+          value: 'Leborn'
+        }
+      }).$mount()
+      const uesElement = vm.$el.querySelector('input')
+      // input的value值怎么是直接获取
+      expect(uesElement.value).to.equal('Leborn')
+      vm.$destroy()
+    })
+
+    it('设置disabled', () => {
+      const Construct = Vue.extend(Input)
+      const vm = new Construct({
+        propsData: {
+          disabled: 'disabled'
+        }
+      }).$mount()
+      const useElement = vm.$el.querySelector('input')
+      expect(useElement.getAttribute('disabled')).to.equal('disabled')
+      vm.$destroy()
+    })
+
+    it('设置readonly', () => {
+      const Construct = Vue.extend(Input);
+      const vm = new Construct({
+        propsData: {
+          readonly: 'readonly'
+        }
+      }).$mount()
+      const useElement = vm.$el.querySelector('input')
+      expect(useElement.getAttribute('readonly')).to.equal('readonly')
+      vm.$destroy()
+    })
+    it('设置error',() => {
+      const Construct = Vue.extend(Input)
+      const vm = new Construct({
+        propsData: {
+          error: '请输入合法内容'
+        }
+      }).$mount()
+      const useElement = vm.$el.querySelector('.errorMessage')
+      expect(useElement).to.exist
+      expect(useElement.innerHTML).to.equal('请输入合法内容')
+      vm.$destroy()
+    })
+
+  })
+
+  // 测试事件
+  describe('func', () => {
+    const Construct = Vue.extend(Input)
+    let vm
+    afterEach(() => {
+      vm.$destroy()
+    })
+    it('支持 change/input/focus/blur 事件', () => {
+      ['change', 'input', 'blur', 'focus'].forEach((eventName, index) => {
+        vm = new Construct({}).$mount()
+        const callback = sinon.fake()
+        vm.$on(eventName, callback)
+        // change/input/focus/blur 事件对象
+        let event = new Event(eventName)
+        // 更改事件对象中target的属性
+        Object.defineProperty(
+          event, 'target', {
+            value: {value: 'hello'}, enumerable: true
+          }
+        )
+        let inputElement = vm.$el.querySelector('input')
+        inputElement.dispatchEvent(event)
+        expect(callback).to.have.been.calledWith('hello')
+      })
+    })
+  })
+
 
 })
