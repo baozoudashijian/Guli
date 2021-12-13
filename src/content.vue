798d75335c8aa08a8cc216469b1e1c77f5fe358d
baozoudashijian <Yhldcahcy521>
2021-12-13 10:22:31 +0800

    完成布局组件

---
+++ src/content.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="g-layout-content">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "content"
+  }
+</script>
+
+<style lang="sass" scoped>
+  .g-layout-content
+    flex: 1 1 auto
+</style>
---
+++ src/header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="g-layout-header">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "header"
+  }
+</script>
+
+<style lang="sass" scoped>
+  .g-layout-header
+    height: 64px
+</style>
