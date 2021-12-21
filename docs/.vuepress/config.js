e354cdc81e72f44c02083834d9b0149c713a0655
baozoudashijian <Yhldcahcy521>
2021-12-21 16:10:32 +0800

    使用vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  title: 'Hello Guli UI',
+  description: 'Just playing around',
+  base: '/',
+  themeConfig: {
+    sidebar: [
+      {
+        title: '快速上手',   // 必要的
+        path: '/guide/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+        collapsable: false, // 可选的, 默认值是 true,
+        sidebarDepth: 0,    // 可选的, 默认值是 1
+        // children: [
+        //   '/'
+        // ]
+      }
+      // {
+      //   title: 'Group 2',
+      //   children: [ /* ... */],
+      //   initialOpenGroupIndex: -1 // 可选的, 默认值是 0
+      // }
+    ]
+    ,
+    nav: [
+      {text: '首页', link: '/'}
+    ]
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,16 @@
+---
+home: true
+heroImage: /images/zhang.jpg
+heroText: Guli UI框架
+tagline: 一个极简的前端UI框架
+actionText: 快速上手 →
+actionLink: /guide/
+features:
+- title: 简洁至上
+  details: 以 Vue 为技术框架封装的UI组件，以最少的配置帮助你专注于项目开发。
+- title: Vue驱动
+  details: 享受 Vue + webpack 的开发体验，在 项目 中使用 Guli 组件，同时可以使用 Vue 来开发自定义主题。
+- title: 高性能
+  details: Guli UI为每个组件都经过千锤百炼，帮助你在开发项目过程中，快速完成任务。
+footer: MIT Licensed | Copyright © 2021-present Ronger Zhang
+---
