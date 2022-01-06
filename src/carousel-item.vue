b8f0ff1c3e94af47e811f69117b90c834d319ec7
baozoudashijian <Yhldcahcy521>
2022-01-06 18:31:36 +0800

    走马灯组件-修改动画添加

--- src/carousel-item.vue
+++ src/carousel-item.vue
@@ -29,10 +29,10 @@
     transition: all .3s ease
 
   .slide-fade-leave-active
-    transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0)
+    transition: all .3s ease
 
-  .slide-fade-enter, .slide-fade-leave-to
+  .slide-fade-enter
     transform: translateX(200px)
-    opacity: 0
-
+  .slide-fade-leave-to
+    transform: translateX(-200px)
 </style>
